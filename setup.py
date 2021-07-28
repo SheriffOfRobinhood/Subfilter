63f0e1cef203e5adb3b0709f5bf3b09dbe18061c
ReadingClouds <p.clark@reading.ac.uk>
2021-07-28 10:29:12 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     # Needed to actually package something
     packages=['subfilter'],
     # Needed for dependencies
-    install_requires=['numpy', 'scipy', 'dask', 'xarray', 'time'],
+    install_requires=['numpy', 'scipy', 'dask', 'xarray'],
     # *strongly* suggested for sharing
     version='0.5.0',
     # The license can be anything you like
